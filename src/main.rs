ca1aab2e1d58de5fd47e8b51caae781f3a937260
Gianluca Scacco <gianluca.scacco@gmail.com>
2024-02-21 11:40:15 +0100

    versione stream

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-use std::{env, fs};
+use std::{env, fs, io::Read};
 
 fn main() {
     let mut v: [u64; 256] = [0; 256];
+    let mut num_bytes = 0;
+    let mut entropy: f32 = 0.0;
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
         println!("USAGE: entropy <filename>");
         return;
     }
     let fpath = args.get(1).unwrap();
-    let buff = fs::read(fpath).unwrap();
-    let nbytes = buff.len();
-    let mut entropy = 0.0;
-    for b in buff {
-        v[b as usize] += 1;
+    let mut file = fs::File::open(fpath).unwrap();
+
+    let mut buffer: [u8; 1] = [0; 1];
+    loop {
+        match file.read_exact(&mut buffer) {
+            Ok(_) => {
+                v[buffer[0] as usize] += 1;
+                num_bytes += 1;
+            }
+            Err(_) => break,
+        }
     }
+
     for value in v {
-        let p = value as f32 / nbytes as f32;
+        let p = value as f32 / num_bytes as f32;
         if p > 0.0 {
             entropy += -1.0 * p * f32::log2(p);
         }
