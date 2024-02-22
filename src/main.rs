32ca69a0e49ef0826652dc7c5652d13f4fd52571
Gianluca Scacco <gianluca.scacco@gmail.com>
2024-02-22 08:47:37 +0100

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::{env, fs, io::Read};
+use std::{fs, io::Read};
 
+use clap::Parser;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: String,
+}
 fn main() {
     let mut v: [u64; 256] = [0; 256];
     let mut num_bytes = 0;
-    let mut entropy: f32 = 0.0;
-    let args: Vec<_> = env::args().collect();
-    if args.len() < 2 {
-        println!("USAGE: entropy <filename>");
-        return;
-    }
-    let fpath = args.get(1).unwrap();
-    let mut file = fs::File::open(fpath).unwrap();
+
+    let args = Args::parse();
+
+    let fpath = args.file;
+    let mut file = fs::File::open(&fpath).unwrap();
 
     let mut buffer: [u8; 1] = [0; 1];
     loop {
@@ -22,13 +27,12 @@ fn main() {
             Err(_) => break,
         }
     }
+    let entropy: f32 = v
+        .iter()
+        .filter(|u| **u != 0)
+        .map(|u| *u as f32 / num_bytes as f32)
+        .map(|u| -1.0 * u * f32::log2(u))
+        .sum();
 
-    for value in v {
-        let p = value as f32 / num_bytes as f32;
-        if p > 0.0 {
-            entropy += -1.0 * p * f32::log2(p);
-        }
-    }
-
-    println!("Shannon entropy: {}", entropy)
+    println!("File {fpath} of size {num_bytes} bytes: Shannon entropy {entropy}")
 }
